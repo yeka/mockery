47da4704d98b114a17d4c98d15209bd6f6b19eef
Daniel Karp & Dave Marshall <danielkarp@gmail.com>
2014-12-18 15:00:15 +0000

    Match positive call expectations signatures

--- library/Mockery/TestDouble/NegativeMethodCallExpectation.php
+++ library/Mockery/TestDouble/NegativeMethodCallExpectation.php
@@ -5,7 +5,27 @@ namespace Mockery\TestDouble;
 interface NegativeMethodCallExpectation 
 {
     /**
+     * @param mixed $arg     The first expected argument
+     * @param mixed $arg,... The subsequent expected arguments
+     *
      * @return void
      */
-    function with();
+    function with($arg/*, $arg2..., $arg3...*/);
+
+    /**
+     * @param array $args The expected arguments
+     *
+     * @return void
+     */
+    function withArgs(array $args);
+
+    /**
+     * @return void
+     */
+    function withNoArgs();
+
+    /**
+     * @return void
+     */
+    function withAnyArgs();
 }
