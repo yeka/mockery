d5ce2080fee9669bbd339ca0101cc819a2a8aaea
Daniel Karp & Dave Marshall <dave@atst.io>
2014-12-18 15:33:42 +0000

    Flesh out StubMethod and associated interfaces

--- library/Mockery/TestDouble/StubMethod.php
+++ library/Mockery/TestDouble/StubMethod.php
@@ -2,10 +2,31 @@
 
 namespace Mockery\TestDouble;
 
-interface StubMethod 
+interface StubMethod extends StubMethodWithArguments
 {
     /**
-     * @void
+     * @param mixed $arg     The first expected argument
+     * @param mixed $arg,... The subsequent expected arguments
+     *
+     * @return StubMethodWithArguments
      */
-    function toReturn();
+    function with($arg/*, $arg2..., $arg3...*/);
+
+    /**
+     * @param array $args The expected arguments
+     *
+     * @return StubMethodWithArguments
+     */
+    function withArgs(array $args);
+
+    /**
+     * @return StubMethodWithArguments
+     */
+    function withNoArgs();
+
+    /**
+     * @return StubMethodWithArguments
+     */
+    function withAnyArgs();
 }
+
---
+++ library/Mockery/TestDouble/StubMethodWithArguments.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mockery\TestDouble;
+
+interface StubMethodWithArguments
+{
+    /**
+     * @void
+     */
+    function toReturn();
+}
