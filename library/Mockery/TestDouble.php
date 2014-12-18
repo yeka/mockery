9f5a9369ee9d2dcec68fe5e45fb8612982e6fcc5
Dave Marshall <dave@atst.io>
2014-12-18 13:45:47 +0000

    Add test double API top level API mock up

---
+++ library/Mockery/TestDouble.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mockery;
+
+use Mockery\TestDouble\Mock;
+use Mockery\TestDouble\Spy;
+use Mockery\TestDouble\Stub;
+use Mockery\TestDouble\Dummy;
+
+class TestDouble 
+{
+    /**
+     * @return Mock
+     */
+    static function mock()
+    {
+
+    }
+
+    /**
+     * @return Spy
+     */
+    static function spy()
+    {
+
+
+    }
+
+    /**
+     * @return Stub
+     */
+    static function stub()
+    {
+
+    }
+
+    /**
+     * @return Dummy
+     */
+    static function dummy()
+    {
+    
+    }
+}
